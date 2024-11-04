ef7c6be0c13300893e3a0a7113d91931c8343d1a
Lucas Correa <correadev@outlook.com>
2024-11-04 07:54:05 -0300

    commit of the complete website
    On this site I used HTML and CSS to build and style the pages; and also JavaScript on the coupons page, to copy the code and display the success message

---
+++ assets/js/cupons.js
@@ -0,0 +1,39 @@
+
+//mensagem de cupom copiado
+document.querySelectorAll('.button').forEach(button => {
+    button.addEventListener('click', function() {
+        event.preventDefault(); // Previne o comportamento padrão do link
+                
+        const couponCode = this.previousElementSibling.innerText; // Pega o código
+        const message = `Código do cupom "${couponCode}" copiado com sucesso!`; // Mensagem personalizada
+        const notification = document.getElementById('notification');
+
+        navigator.clipboard.writeText(couponCode).then(() => {
+            notification.innerText = message; // Atualiza a mensagem
+            notification.style.display = 'block'; // Mostra a notificação
+            setTimeout(() => {
+                notification.style.opacity = '0'; // Adiciona efeito de fade-out
+                setTimeout(() => {
+                    notification.style.display = 'none'; // Oculta após o fade-out
+                    notification.style.opacity = '1'; // Reseta a opacidade
+                }, 500);
+            }, 2000); // Mostra a notificação por 2 segundos
+        }).catch(err => {
+            console.error('Erro ao copiar o código: ', err);
+            notification.innerText = 'Ocorreu um erro ao copiar o código.'; // Mensagem de erro
+            notification.style.display = 'block'; // Mostra a notificação
+            setTimeout(() => {
+                notification.style.opacity = '0'; // Fade-out
+                setTimeout(() => {
+                    notification.style.display = 'none'; // Oculta após o fade-out
+                    notification.style.opacity = '1'; // Reseta a opacidade
+                }, 500);
+            }, 2000); // Mostra a notificação de erro por 2 segundos
+        });
+    });
+});
+
+//apenas um alerta para o console do navegador, para carregamento em resoluções menores
+document.addEventListener('DOMContentLoaded', function(){
+    console.log("Página carregada")
+})
